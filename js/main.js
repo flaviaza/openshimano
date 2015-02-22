0a425cd7dd172b3cb4011e1d6b47842d9e1d6125
Franco Catena <francocatena@gmail.com>
2015-02-22 20:58:04 -0300

    Detect transform support

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
+var transformSupported = function () {
+  var prefixes = ['transform', 'WebkitTransform', 'MozTransform', 'OTransform', 'msTransform']
+  var div      = document.createElement('div')
+
+  for (var i = 0; i < prefixes.length; i++)
+    if (div && div.style[prefixes[i]] !== undefined) return true
+
+  return false
+}
+
 jQuery(function ($) {
   var animate = true
 
   $.stellar({
-    positionProperty: 'transform'
+    positionProperty: transformSupported() ? 'transform' : 'position'
   })
 
   $('[data-footer]').on('mouseenter', function () {

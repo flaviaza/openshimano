74d577a696a989c1d48edf748d436fd11c9f2327
flaviaza <flavia.ibanez@gmail.com>
2015-03-12 18:35:23 -0400

    adding smooth scrolling

--- js/main.js
+++ js/main.js
@@ -26,4 +26,21 @@ jQuery(function ($) {
   $(document).on('affixed-top.bs.affix', '[data-footer]', function () {
     animate = true
   })
+
+  // SMOOTH SCROLLING
+  // copied from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+    }
+  })
+
 })
